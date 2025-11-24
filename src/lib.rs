10784e87fe620d25f8fdd2f6edcb11c1178b38a0
Lexo Liu <me@lexo.cool>
2025-11-24 12:59:54 +0800

    Add search integration to Zenflow theme

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use thought_plugin::{
     askama::Template,
     export_theme,
-    helpers::{article_output_file, format_rfc3339, markdown_to_html},
+    helpers::{
+        article_output_file, format_rfc3339, markdown_to_html, search_script_for_article,
+        search_script_path,
+    },
     Article, ArticlePreview, Theme,
 };
 
@@ -11,8 +14,13 @@ pub struct Zenflow;
 #[template(path = "article.html")]
 struct PageTemplate<'a> {
     title: &'a str,
+    site_title: &'a str,
+    footer: &'a str,
     created: &'a str,
     body: &'a str,
+    author: &'a str,
+    asset_prefix: &'a str,
+    search_js: &'a str,
 }
 
 struct IndexEntry {
@@ -24,17 +32,29 @@ struct IndexEntry {
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
     entries: &'a [IndexEntry],
+    site_title: &'a str,
+    footer: &'a str,
+    asset_prefix: &'a str,
+    search_js: &'a str,
 }
 
 impl Theme for Zenflow {
     fn generate_page(article: Article) -> String {
         let html_output = markdown_to_html(article.content());
         let created = format_rfc3339(article.metadata().created());
+        let depth = article.preview().category().path().len();
+        let asset_prefix = relative_prefix(depth);
+        let search_js = search_script_for_article(article.preview());
 
         PageTemplate {
             title: article.title(),
+            site_title: article.preview().site_title(),
+            footer: article.preview().site_footer(),
             created: &created,
+            author: article.preview().metadata().author(),
             body: html_output.as_str(),
+            asset_prefix: &asset_prefix,
+            search_js: &search_js,
         }
         .render()
         .expect("failed to render page template")
@@ -48,11 +68,27 @@ impl Theme for Zenflow {
                 href: article_output_file(&article),
             });
         }
+        let search_js = search_script_path();
+        let asset_prefix = ".";
 
-        IndexTemplate { entries: &entries }
-            .render()
-            .expect("failed to render index template")
+        IndexTemplate {
+            entries: &entries,
+            site_title: "Zenflow",
+            footer: "Thought",
+            asset_prefix,
+            search_js,
+        }
+        .render()
+        .expect("failed to render index template")
     }
 }
 
 export_theme!(Zenflow);
+
+fn relative_prefix(depth: usize) -> String {
+    if depth == 0 {
+        ".".to_string()
+    } else {
+        "../".repeat(depth)
+    }
+}
